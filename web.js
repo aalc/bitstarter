f8b1fcf366c28c8d0dfe2a7e978c9aca37f36120
aalc <cs.aal.c@gmail.com>
2013-07-11 02:30:49 +0000

    web.js reads index.html

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ var helloFile = fs.readFileSync(require('path').resolve(__dirname, 'index.html'),'utf8');    
+ response.send(helloFile);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
